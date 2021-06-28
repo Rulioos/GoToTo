c4ad39888705dab65d15744a2991012ad217acb2
Aubier Jules <33544567+Rulioos@users.noreply.github.com>
2021-06-28 15:52:13 +1100

    Feat: CMD commands to generate conf and ts files

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
-	basepath := "./test"
-	outputDir := "./output"
-	GenerateConfigYaml(basepath, outputDir)
-	GenerateFromYaml(basepath)
+
+	GenerateConfigYamlCmd := flag.NewFlagSet("generateYML", flag.ExitOnError) //Generate config file
+	outputDir := GenerateConfigYamlCmd.String("dir", "./output", "Directory in which the .ts files will be created")
+	scanPath := GenerateConfigYamlCmd.String("spath", "./", "Path to the project to scan")
+
+	GenerateFromYamlCmd := flag.NewFlagSet("generateTS", flag.ExitOnError) //Generate ts files
+	projectPath := GenerateFromYamlCmd.String("spath", "./", "Path to the project to scan")
+
+	if len(os.Args) < 1 {
+		fmt.Println("expected 'generateYML' or 'generateTS' subcommands")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "generateYML":
+		GenerateConfigYamlCmd.Parse(os.Args[2:])
+		fmt.Printf("outputDir: %v\n", *outputDir)
+		fmt.Printf("scanPath: %v\n", *scanPath)
+	case "generateTS":
+		GenerateFromYamlCmd.Parse(os.Args[2:])
+		fmt.Printf("projectPath: %v\n", *projectPath)
+	default:
+		fmt.Println("expected 'generateYML' or 'generateTS' subcommands")
+		os.Exit(1)
+	}
 
 }
